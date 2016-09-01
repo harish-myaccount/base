9b1843ef429399d432075327ba78d2cf402ed6a3
Harish Reddy <harish.myemails@gmail.com>
2016-09-01 12:50:00 +0530

    hello page

--- src/main/java/com/squad/cccreview/controller/HelloController.java
+++ src/main/java/com/squad/cccreview/controller/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
 
-	@RequestMapping("/")
+	@RequestMapping("/hello")
 	public String hello() {
 		return "hello";
 	}
